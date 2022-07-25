0e744f71d3e3d3558a8a09bff3ce5289e26f2c0b
talaxasy <talaxasy.or.lev@gmail.com>
2022-07-25 14:08:51 +0300

    update ui of post page

--- src/pages/Post.vue
+++ src/pages/Post.vue
@@ -1,20 +1,38 @@
 
 
 <script setup lang="ts">
-import { computed } from '@vue/reactivity';
+import redaxios from 'redaxios';
+import { onMounted, reactive } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 
 const route = useRoute();
+const router = useRouter();
 
-const id = computed(() => {
-    return route.params.id;
+const state = reactive<{
+    post: { title: string, description: string } | null;
+    loading: boolean;
+}>({
+    post: null,
+    loading: false,
+});
+
+onMounted(async () => {
+    await redaxios(`https://jsonplaceholder.typicode.com/posts/${route.params.id}`)
+        .then(({ data }) => state.post = { title: (data as any).title, description: (data as any).body })
+        .catch(() => router.push('/404'));
+    state.loading = false;
 })
 
 </script>
 
 <template>
-    <div>
-        {{ $route.params.id }}
+    <div v-if="!state.loading">
+        <h1>{{ state.post?.title }}</h1>
+        <br>
+        <p>{{ state.post?.description }}</p>
+    </div>
+    <div v-else>
+        loading...
     </div>
 </template>
 
